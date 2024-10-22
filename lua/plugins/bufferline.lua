b7ef7fce334515278c3e7d7a5bd2f5b9c5d1171f
Quentin Lapierre <quentin.lapierre@epitech.eu>
2024-10-23 00:25:18 +0200

    [EDIT] Bufferline keymaps
    Added:
    - Toggle bufferline
    
    Fix:
    - Styling

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,14 +4,23 @@ return {
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
     vim.opt.termguicolors = true
+
+    local function toggle_bufferline()
+      if vim.o.showtabline == 0 then
+        vim.o.showtabline = 2
+      else
+        vim.o.showtabline = 0
+      end
+    end
+
     -- Keymaps
     vim.keymap.set("n", "<C-o>", "<Cmd>BufferLineCycleNext<CR>", { noremap = true, desc = "" })
     vim.keymap.set("n", "<C-i>", "<Cmd>BufferLineCyclePrev<CR>", { noremap = true, desc = "" })
-    -- vim.keymap.set('', '', '', { noremap = true, desc = '' })
+    vim.keymap.set("n", "<leader>tt", toggle_bufferline, { noremap = true, desc = "Show/Hide bufferline" })
 
     require("bufferline").setup({
       options = {
-        show_tab_indicators = true,
+        show_tab_indicators = false,
         offsets = {
           {
             filetype = "neo-tree",
@@ -21,8 +30,7 @@ return {
           },
         },
         show_close_icon = false,
-        show_buffer_close_icons = true,
-        separator_style = "slope",
+        show_buffer_close_icons = false,
       },
     })
 
