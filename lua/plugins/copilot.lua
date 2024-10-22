1e365bde0f5a9aff8d4da7b80c7098417ea2d457
Quentin Lapierre <quentin.lapierre@epitech.eu>
2024-10-23 00:26:00 +0200

    [ADD] Added github copilot
    Added:
    - Github copilot (completions)
    - Github copilot chat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,35 @@
 return {
-	"github/copilot.vim",
+	{
+		"github/copilot.vim",
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "github/copilot.vim" },
+			{ "nvim-lua/plenary.nvim" },
+		},
+		build = "make tiktoken",
+		opts = {
+			debug = false,
+			window = {
+				layout = "float",
+				border = "double",
+				title = "Copilot Chat",
+			},
+		},
+		config = function(_, opts)
+			local chat = require("CopilotChat")
+			chat.setup(opts)
+
+			vim.keymap.set({ "n", "v" }, "<leader>gc", chat.toggle, { noremap = true })
+			vim.keymap.set({ "n", "v" }, "<leader>ga", function()
+				chat.ask("Show me something interesting", {
+					callback = function(response)
+						print("Response:", response)
+					end,
+				})
+			end, { noremap = true })
+		end,
+	},
 }
