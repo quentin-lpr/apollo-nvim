f94126cabd5eedb5aa546217e05812b2ce1eafef
Quentin Lapierre <quentin.lapierre@epitech.eu>
2024-10-23 22:04:40 +0200

    [ADD] Mini align
    Added:
    - Global file for all "mini.nvim" related plugins

---
+++ lua/plugins/mini-nvim.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "echasnovski/mini.animate",
+    version = false,
+    config = function()
+      require("mini.animate").setup({
+        cursor = {
+          timing = function(_, n)
+            return 25 / n
+          end,
+        },
+        scroll = {
+          timing = function(_, n)
+            return 50 / n
+          end,
+        },
+        resize = {
+          timing = function(_, n)
+            return 25 / n
+          end,
+        },
+        open = {
+          timing = function(_, n)
+            return 25 / n
+          end,
+        },
+        close = {
+          timing = function(_, n)
+            return 25 / n
+          end,
+        },
+      })
+    end,
+  },
+  {
+    "echasnovski/mini.align",
+    version = false,
+    config = function()
+      require("mini.align").setup()
+    end,
+  },
+}
