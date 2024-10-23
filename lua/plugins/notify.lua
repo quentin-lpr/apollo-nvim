a6399b0d749cb12449f8fb572dc4635415474213
Quentin L. <skrillex32842518742@gmail.com>
2024-10-23 12:52:48 +0200

    [ADD] Notify plugin for better notifications

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,43 @@
+return {
+  "rcarriga/nvim-notify",
+  config = function()
+    vim.notify = require("notify").setup({
+      fps = 60,
+      level = 2,
+      minimum_width = 30,
+      max_width = 50,
+      render = "wrapped-compact",
+      stages = "static",
+      timeout = 1500,
+      icons = {
+        ERROR = "",
+        WARN = "",
+        INFO = "",
+        DEBUG = "",
+        TRACE = "",
+      },
+    })
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      pattern = "*",
+      callback = function()
+        vim.cmd([[
+          highlight NotifyERRORBorder guifg=#8A1F1F
+          highlight NotifyERRORIcon guifg=#F70067
+          highlight NotifyERRORTitle  guifg=#F70067
+          highlight NotifyWARNBorder guifg=#79491D
+          highlight NotifyWARNIcon guifg=#F79000
+          highlight NotifyWARNTitle guifg=#F79000
+          highlight NotifyINFOBorder guifg=#808080
+          highlight NotifyINFOIcon guifg=#808080
+          highlight NotifyINFOTitle guifg=#808080
+          highlight NotifyDEBUGBorder guifg=#8B8B8B
+          highlight NotifyDEBUGIcon guifg=#8B8B8B
+          highlight NotifyDEBUGTitle  guifg=#8B8B8B
+          highlight NotifyTRACEBorder guifg=#4F3552
+          highlight NotifyTRACEIcon guifg=#D484FF
+          highlight NotifyTRACETitle  guifg=#D484FF
+        ]])
+      end,
+    })
+  end,
+}
