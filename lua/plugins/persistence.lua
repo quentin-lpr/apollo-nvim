2fc794e9f1bd843ff26b213648fa31482ffd4b6d
Quentin Lapierre <quentin.lapierre@epitech.eu>
2024-10-26 01:32:22 +0200

    [ADD] Session management plugin

---
+++ lua/plugins/persistence.lua
@@ -0,0 +1,27 @@
+-- Simple session management for Neovim
+return {
+  "folke/persistence.nvim",
+  event = "BufReadPre",
+  opts = {},
+  config = function()
+    require("persistence").setup({
+      dir = vim.fn.stdpath("state") .. "/sessions/",
+      need = 1,
+      branch = true,
+    })
+    local builtin = require("persistence")
+
+    local wk = require("which-key")
+    wk.add({
+      {
+        group = "Session",
+        "<leader>q",
+        -- { "<leader>qs", builtin.load,   mode = "n", desc = "Load last session" },
+        { "<leader>qS", builtin.select, mode = "n", desc = "Select session to load" },
+        { "<leader>ql", builtin.load,   mode = "n", desc = "Load last session" },
+        { "<leader>qd", builtin.stop,   mode = "n", desc = "Stop persistence" },
+        icon = { icon = "", color = "white" },
+      },
+    })
+  end,
+}
