cf500dd70b962ad5678770ad25ebc40d7c263a47
Quentin Lapierre <quentin.lapierre@epitech.eu>
2024-10-25 02:27:49 +0200

    [FIX] Keymaps using which-key
    Fix:
    - Changed keymaps binding using "vim.keymap.set" to which-key's one

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,53 +1,64 @@
 return {
-  "akinsho/bufferline.nvim",
-  version = "*",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    vim.opt.termguicolors = true
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		vim.opt.termguicolors = true
 
-    local function toggle_bufferline()
-      if vim.o.showtabline == 0 then
-        vim.o.showtabline = 2
-      else
-        vim.o.showtabline = 0
-      end
-    end
+		local function toggle_bufferline()
+			if vim.o.showtabline == 0 then
+				vim.o.showtabline = 2
+			else
+				vim.o.showtabline = 0
+			end
+		end
 
-    -- Keymaps
-    vim.keymap.set("n", "<C-o>", "<Cmd>BufferLineCycleNext<CR>", { noremap = true, desc = "" })
-    vim.keymap.set("n", "<C-i>", "<Cmd>BufferLineCyclePrev<CR>", { noremap = true, desc = "" })
-    vim.keymap.set("n", "<leader>tt", toggle_bufferline, { noremap = true, desc = "Show/Hide bufferline" })
+		-- Keymaps
+		local wk = require("which-key")
+		wk.add({
+			{ "<C-o>", "<Cmd>BufferLineCycleNext<CR>", mode = "n", desc = "Next buffer", icon = { icon = "", color = "white" } },
+			{ "<C-i>", "<Cmd>BufferLineCyclePrev<CR>", mode = "n", desc = "Previous buffer", icon = { icon = "", color = "white" } },
+			{ "<leader>t", toggle_bufferline, mode = "n", desc = "Show/Hide bufferline", icon = { icon = "", color = "white" } },
+		})
 
-    require("bufferline").setup({
-      options = {
-        show_tab_indicators = false,
-        offsets = {
-          {
-            filetype = "neo-tree",
-            text = "File Explorer",
-            separator = false,
-            text_align = "center",
-          },
-        },
-        show_close_icon = false,
-        show_buffer_close_icons = false,
-      },
-    })
+		require("bufferline").setup({
+			options = {
+				show_tab_indicators = false,
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "File Explorer",
+						separator = false,
+						text_align = "center",
+					},
+				},
+				show_close_icon = false,
+				show_buffer_close_icons = false,
+			},
+		})
 
-    -- Ajout de l'autocommand pour masquer la bufferline sur l'écran alpha
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "alpha",
-      callback = function()
-        vim.opt_local.showtabline = 0
-      end,
-    })
+		-- local function update_bufferline()
+		-- 	local buf_count = #vim.fn.getbufinfo({ buflisted = 1 })
+		-- 	print("add")
+		-- 	vim.opt.showtabline = (buf_count > 1) and 2 or 0
+		-- end
 
-    -- Restaurer la bufferline lorsqu'on quitte l'écran alpha
-    vim.api.nvim_create_autocmd("BufUnload", {
-      buffer = 0,
-      callback = function()
-        vim.opt.showtabline = 2
-      end,
-    })
-  end,
+		-- vim.api.nvim_create_autocmd({ "BufAdd", "BufRead" }, {
+		-- 	callback = update_bufferline,
+		-- })
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "alpha",
+			callback = function()
+				vim.opt.showtabline = 0
+			end,
+		})
+
+		vim.api.nvim_create_autocmd("BufUnload", {
+			buffer = 0,
+			callback = function()
+				vim.opt.showtabline = 2
+			end,
+		})
+	end,
 }
