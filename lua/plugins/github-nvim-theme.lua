3b031cd6a95f77694cdbe22ed654546d21a4e698
Quentin Lapierre <quentin.lapierre@epitech.eu>
2024-10-25 02:33:04 +0200

    [FIX] UI
    Fix:
    - Set panes to right/bottom instead of left/top
    - Lazy loading "noice" to fix UI problems
    
    Added:
    - Italic comments to gituhb theme

--- lua/plugins/github-nvim-theme.lua
+++ lua/plugins/github-nvim-theme.lua
@@ -1,4 +1,13 @@
 return {
-  "projekt0n/github-nvim-theme",
-  name = "github-theme",
+	"projekt0n/github-nvim-theme",
+	name = "github-theme",
+	config = function()
+		require("github-theme").setup({
+			options = {
+				styles = {
+					comments = "italic",
+				},
+			},
+		})
+	end,
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,4 +4,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number relativenumber")
 vim.cmd("set nu rnu")
+vim.cmd("set splitbelow")
+vim.cmd("set splitright")
+vim.opt.showtabline = 0
 vim.cmd.colorscheme("github_dark_default")
