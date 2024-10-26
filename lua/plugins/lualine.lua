afc3fd9c17bff53a8ef1efcd388be9cf3c017f21
Quentin Lapierre <quentin.lapierre@epitech.eu>
2024-10-27 01:03:15 +0200

    [FIX] Removed unused setup functions

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,23 +1,21 @@
 -- A blazing fast and easy to configure neovim statusline
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "iceberg_dark",
-      },
-      sections = {
-        lualine_c = {
-          {
-            "filename",
-            file_status = true,
-            newfile_status = false,
-            path = 4,
-          },
-        },
-        lualine_x = { "encoding", "filetype" },
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	opts = {
+		options = {
+			theme = "iceberg_dark",
+		},
+		sections = {
+			lualine_c = {
+				{
+					"filename",
+					file_status = true,
+					newfile_status = false,
+					path = 4,
+				},
+			},
+			lualine_x = { "encoding", "filetype" },
+		},
+	},
 }
