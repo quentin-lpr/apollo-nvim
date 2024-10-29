ca2c786d4efc143971745c405cc8ace46a9a1773
Quentin Lapierre <quentin.lapierre@epitech.eu>
2024-10-30 00:25:18 +0100

    [ADD] Whichh-key icon to the "surround-ui" plugin

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,22 +1,20 @@
 -- WhichKey helps you remember your Neovim keymaps, by showing available keybindings in a popup as you type
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	opts = {},
-	config = function()
-		local wk = require("which-key")
-		wk.setup({
-			preset = "helix",
-			icons = {
-				group = "",
-				ellipsis = "…",
-				mappings = true,
-				rules = {},
-				colors = true,
-			},
-			mappings = true,
-		})
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {},
+  config = function()
+    local wk = require("which-key")
+    wk.setup({
+      preset = "helix",
+      icons = {
+        group = "",
+        rules = {
+          { pattern = "surround", icon = "󰅪", color = "white" },
+        },
+      },
+    })
 
-		wk.add({ "<leader>?", "<Cmd>WhichKey<CR>", desc = "Show local keymaps", icon = { icon = "󰋖", color = "white" } })
-	end,
+    wk.add({ "<leader>?", "<Cmd>WhichKey<CR>", desc = "Show local keymaps", icon = { icon = "󰋖", color = "white" } })
+  end,
 }
