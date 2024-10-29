961a09b059700edce5c29673caefdd5f585294ef
Quentin Lapierre <quentin.lapierre@epitech.eu>
2024-10-29 17:57:24 +0100

    [ADD] Added surround-ui

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -1,7 +1,21 @@
--- Add/change/delete surrounding delimiter pairs with ease
 return {
-	"kylechui/nvim-surround",
-	event = "VeryLazy",
-	version = "*",
-	opts = {},
+  -- Add/change/delete surrounding delimiter pairs with ease
+  {
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    version = "*",
+    opts = {},
+  },
+  -- A Neovim plugin which acts as a helper or training aid for kylechui/nvim-surround
+  {
+    "roobert/surround-ui.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "kylechui/nvim-surround",
+      "folke/which-key.nvim",
+    },
+    opts = {
+      root_key = "s",
+    },
+  },
 }
