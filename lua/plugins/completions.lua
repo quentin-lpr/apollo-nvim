f45b5c0c81e421c0354f69c154ced85661bfd5fc
Quentin L. <skrillex32842518742@gmail.com>
2024-10-30 14:31:11 +0100

    [FIX] Optimized startup time

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,9 +1,11 @@
 return {
 	{
 		"hrsh7th/cmp-nvim-lsp",
+		event = "BufReadPre",
 	},
 	{
 		"L3MON4D3/LuaSnip",
+		event = "BufReadPre",
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
@@ -11,6 +13,7 @@ return {
 	},
 	{
 		"hrsh7th/nvim-cmp",
+		event = "BufReadPre",
 		config = function()
 			local cmp = require("cmp")
 			require("luasnip.loaders.from_vscode").lazy_load()
