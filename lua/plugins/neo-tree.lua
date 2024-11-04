7124a078bf038f41144fc3584a3bbe7323afacdb
Quentin L. <skrillex32842518742@gmail.com>
2024-11-04 10:01:43 +0100

    [FIX] Changed neo-tree keybinds

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,33 +1,42 @@
 -- Neovim plugin to manage the file system and other tree like structures
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	event = "VeryLazy",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("neo-tree").setup({
-			window = {
-				width = 25,
-				mappings = {
-					["H"] = "toggle_hidden",
-					["<tab>"] = "open",
-					["l"] = "set_root",
-					["h"] = "navigate_up",
-					["<C-k>"] = { "scroll_preview", config = { direction = 2 } },
-					["<C-j>"] = { "scroll_preview", config = { direction = -2 } },
-					["s"] = "open_rightbelow_vs",
-				},
-			},
-		})
+  "nvim-neo-tree/neo-tree.nvim",
+  event = "VeryLazy",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      window = {
+        width = 25,
+        mappings = {
+          ["H"] = "toggle_hidden",
+          ["<tab>"] = "open",
+          ["l"] = "set_root",
+          ["h"] = "navigate_up",
+          ["<C-k>"] = { "scroll_preview", config = { direction = 2 } },
+          ["<C-j>"] = { "scroll_preview", config = { direction = -2 } },
+          ["s"] = "open_rightbelow_vs",
+        },
+      },
+    })
 
-		local wk = require("which-key")
-		wk.add({
-			{ "<C-@>", "<Cmd>Neotree toggle left<CR><Esc>", mode = "n", desc = "Show Nvim File Tree", icon = { icon = "󰙅", color = "white" } },
-			{ "<leader>e", "<Cmd>Neotree focus<CR><Esc>", mode = "n", desc = "Focus neo-tree", icon = { icon = "󰙅", color = "white" } },
-		})
-	end,
+    local os_name = vim.loop.os_uname().sysname
+    local wk = require("which-key")
+    wk.add({
+      { "<leader>e", "<Cmd>Neotree focus<CR><Esc>", mode = "n", desc = "Focus neo-tree", icon = { icon = "󰙅", color = "white" } },
+    })
+    if os_name == "Windows_NT" then
+      wk.add({
+        { "<leader>²", "<Cmd>Neotree toggle left<CR><Esc>", mode = "n", desc = "Show Nvim File Tree", icon = { icon = "󰙅", color = "white" } },
+      })
+    elseif os_name == "Darwin" then
+      wk.add({
+        { "<leader>@", "<Cmd>Neotree toggle left<CR><Esc>", mode = "n", desc = "Show Nvim File Tree", icon = { icon = "󰙅", color = "white" } },
+      })
+    end
+  end,
 }
