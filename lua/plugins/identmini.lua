83e6d5ddf53ade6ba4adf666f3ad4cdd9bf94af7
Quentin L. <skrillex32842518742@gmail.com>
2024-11-05 14:35:39 +0100

    [ADD] Improved UX
    Added:
    - Indentmini for better identations
    - Smartcolumn to show a colorcolumn

---
+++ lua/plugins/identmini.lua
@@ -0,0 +1,9 @@
+return {
+  "nvimdev/indentmini.nvim",
+  event = "BufReadPre",
+  config = function()
+    require("indentmini").setup()
+    vim.cmd.highlight("IndentLine guifg=#2c2c2c")
+    vim.cmd.highlight("IndentLineCurrent guifg=#4c4c4c")
+  end,
+}
---
+++ lua/plugins/smartcolumn.lua
@@ -0,0 +1,8 @@
+return {
+	"m4xshen/smartcolumn.nvim",
+  event = "BufReadPre",
+	opts = {
+		colorcolumn = "160",
+		disabled_filetypes = { "alpha", "lazy", "help" },
+	},
+}
