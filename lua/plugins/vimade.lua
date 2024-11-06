21bc85d89daaff057b355734c1a55fb6a30f84cd
Quentin Lapierre <quentin.lapierre@epitech.eu>
2024-11-06 01:07:55 +0100

    [FIX] Disabled unused plugins

--- lua/plugins/vimade.lua
+++ lua/plugins/vimade.lua
@@ -1,6 +1,7 @@
 -- An eye friendly plugin that fades your inactive buffers and preserves your syntax highlighting
 return {
   "TaDaa/vimade",
+  enabled = false,
   event = "VeryLazy",
   opts = {
     fadelevel = 0.5,
