89402c61c33729861edb2b7ce3c589903a10e41c
Quentin L. <skrillex32842518742@gmail.com>
2024-11-06 17:56:36 +0100

    [FIX] Changed colorcolumn color

--- lua/plugins/smartcolumn.lua
+++ lua/plugins/smartcolumn.lua
@@ -1,8 +1,11 @@
 return {
 	"m4xshen/smartcolumn.nvim",
-  event = "BufReadPre",
-	opts = {
-		colorcolumn = "160",
-		disabled_filetypes = { "alpha", "lazy", "help" },
-	},
+	event = "BufReadPre",
+	config = function()
+		require("smartcolumn").setup({
+			colorcolumn = "160",
+			disabled_filetypes = { "alpha", "lazy", "help" },
+		})
+		vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#1c1c1c" })
+	end,
 }
