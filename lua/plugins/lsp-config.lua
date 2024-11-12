ba92523e51d44e56def672883b5cea556092caa8
Quentin Lapierre <quentin.lapierre@epitech.eu>
2024-11-12 02:01:47 +0100

    [ADD] Clangd LSP

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -31,6 +31,7 @@ return {
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
+      lspconfig.clangd.setup({ capabilities = capabilities })
       lspconfig.lua_ls.setup({ capabilities = capabilities })
       lspconfig.ts_ls.setup({
         capabilities = capabilities,
