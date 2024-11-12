47689d6dfbaa334bcc8249c7fba3312a1861aa0a
Quentin Lapierre <quentin.lapierre@epitech.eu>
2024-11-12 02:01:57 +0100

    [EDIT] trouble now focus on open

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,16 +1,19 @@
 -- A pretty diagnostics, references, telescope results, quickfix and location list to help you solve all the trouble your code is causing
 return {
-	"folke/trouble.nvim",
-	event = "BufReadPre",
-	config = function()
-		require("trouble").setup()
+  "folke/trouble.nvim",
+  event = "BufReadPre",
+  opts = {
+    focus = true,
+  },
+  config = function(_, opts)
+    require("trouble").setup(opts)
 
-		local wk = require("which-key")
-		wk.add({
-			group = "Trouble",
-			"<leader>t",
-			{ "<leader>tt", "<Cmd>Trouble diagnostics toggle<CR>", mode = "n", desc = "Toggle diagnostics" },
-			icon = { icon = "", color = "white" },
-		})
-	end,
+    local wk = require("which-key")
+    wk.add({
+      group = "Trouble",
+      "<leader>t",
+      { "<leader>tt", "<Cmd>Trouble diagnostics toggle<CR>", mode = "n", desc = "Toggle diagnostics" },
+      icon = { icon = "", color = "white" },
+    })
+  end,
 }
