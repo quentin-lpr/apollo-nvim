9b6268381f1161cd39e764a32c48273463980d94
Quentin Lapierre <quentin.lapierre@epitech.eu>
2025-01-13 16:29:05 +0100

    [STYLE] Format

--- lua/plugins/xcode.lua
+++ lua/plugins/xcode.lua
@@ -1,10 +1,10 @@
 return {
   "wojciech-kulik/xcodebuild.nvim",
-  dependencies = {
-    -- "stevearc/oil.nvim", -- (optional) to manage project files
-  },
+  enabled = false,
+  -- dependencies = {
+  -- "stevearc/oil.nvim", -- (optional) to manage project files
+  -- },
   config = function()
-    require("xcodebuild").setup({
-    })
+    require("xcodebuild").setup()
   end,
 }
