2856ffa7d7a9e19b4e1d0fdc3ed8479cdb9676fa
Quentin Lapierre <quentin.lapierre@epitech.eu>
2025-01-20 11:17:01 +0100

    edit: Disabled sourcekit

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -31,7 +31,7 @@ return {
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.sourcekit.setup({ capabilities = capabilities })
+      -- lspconfig.sourcekit.setup({ capabilities = capabilities })
       lspconfig.clangd.setup({ capabilities = capabilities })
       lspconfig.lua_ls.setup({ capabilities = capabilities })
       lspconfig.ts_ls.setup({
