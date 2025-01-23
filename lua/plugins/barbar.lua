d24126d1d523ef2f87a4e73c276ed1457d4baee4
Quentin Lapierre <quentin.lapierre@epitech.eu>
2025-01-23 10:49:38 +0100

    [EDIT] Revert keymaps

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -31,8 +31,8 @@ return {
     -- Keymaps
     local wk = require("which-key")
     wk.add({
-      { "<C-u>", "<Cmd>BufferPrevious<CR>", mode = "n", desc = "Previous buffer", icon = { icon = "", color = "white" } },
-      { "<C-i>", "<Cmd>BufferNext<CR>", mode = "n", desc = "Next buffer", icon = { icon = "", color = "white" } },
+      { "<C-i>", "<Cmd>BufferPrevious<CR>", mode = "n", desc = "Previous buffer", icon = { icon = "", color = "white" } },
+      { "<C-o>", "<Cmd>BufferNext<CR>", mode = "n", desc = "Next buffer", icon = { icon = "", color = "white" } },
       { "<leader>bt", toggle_bufferline, mode = "n", desc = "Toggle bufferline", icon = { icon = "", color = "white" } },
       { "<leader>bp", "<Cmd>BufferLinePick<CR>", mode = "n", desc = "Pick buffer", icon = { icon = "󰝁", color = "white" } },
       { "<C-x>", "<Cmd>BufferClose<CR>", mode = { "n", "i" }, desc = "Delete current buffer", icon = { icon = "󱧖", color = "white" } },
