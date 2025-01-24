d9ab4c5909208ef2430fca7e33a0b074cd824058
Quentin Lapierre <quentin.lapierre@epitech.eu>
2025-01-24 10:13:52 +0100

    [ADD] Specific clangd version

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -33,7 +33,12 @@ return {
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
       -- lspconfig.sourcekit.setup({ capabilities = capabilities })
-      lspconfig.clangd.setup({ capabilities = capabilities })
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+        init_options = {
+          fallbackFlags = {'--std=c++20'}
+        },
+      })
       lspconfig.lua_ls.setup({ capabilities = capabilities })
       lspconfig.ts_ls.setup({
         capabilities = capabilities,
