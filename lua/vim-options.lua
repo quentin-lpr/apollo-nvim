5c7fe9b89136c32d4a79a416f01f157367d881f7
Quentin Lapierre <quentin.lapierre@epitech.eu>
2025-03-27 15:31:03 +0100

    feat(filetype): add docker-compose detection
    Configure Neovim to automatically set yaml.docker-compose filetype
    when editing docker-compose.yml/yaml files, enabling proper syntax
    highlighting and filetype-specific features.

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -12,3 +12,9 @@ vim.cmd("set nowrap")
 vim.cmd("set laststatus=0")
 vim.cmd.colorscheme("catppuccin-frappe")
 
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+  pattern = {"docker-compose.yml", "docker-compose.yaml"},
+  callback = function()
+    vim.bo.filetype = "yaml.docker-compose"
+  end
+})
