2b40db527efd0d74ea43205ff045effcb7a2d147
Quentin Lapierre <quentin.lapierre@epitech.eu>
2025-03-31 15:35:43 +0200

    refactor(lsp): update diagnostic config to new API
    Replace deprecated LSP diagnostics handler with the modern
    vim.diagnostic.config() API. This simplifies the configuration and
    removes the redundant underline setting.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -98,11 +98,10 @@ return {
         },
       })
 
-      vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+      vim.diagnostic.config({
         virtual_text = true,
         signs = false,
         update_in_insert = true,
-        underline = true,
       })
     end,
   },
