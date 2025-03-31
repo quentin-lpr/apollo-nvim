101923f1d5fa5f7fe947c007f639a9e65eb69b88
Quentin Lapierre <quentin.lapierre@epitech.eu>
2025-03-31 16:26:18 +0200

    fix(snippets): enable Tab navigation between placeholders
    Previously Tab/S-Tab keys weren't properly configured to navigate
    between snippet placeholders, only allowing completion navigation.
    
    This change:
    - Adds LuaSnip require statement
    - Configures Tab/S-Tab keys to jump between snippet placeholders
    - Falls back to default behavior when no snippet is active

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -24,6 +24,7 @@ return {
     config = function()
       local cmp = require("cmp")
       local lspkind = require("lspkind")
+      local luasnip = require("luasnip")
       require("luasnip.loaders.from_vscode").lazy_load()
       require("vim-react-snippets").lazy_load()
 
@@ -59,15 +60,27 @@ return {
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(1) then
+              luasnip.jump(1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
           { name = "luasnip" },
         }, {
-            { name = "buffer" },
-          }),
+          { name = "buffer" },
+        }),
       })
     end,
   },
