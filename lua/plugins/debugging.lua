44c108a0035658fa5d8d9e328829b01188519ed6
Quentin Lapierre <quentin.lapierre@epitech.eu>
2025-04-14 17:53:39 +0200

    style(nvim config): Format debugging plugin setup
    Reformat the debugging plugin configuration file for consistency.
    No functional changes were made.

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,73 +1,73 @@
 return {
-	{
-		"mfussenegger/nvim-dap",
-		event = "VeryLazy",
-		config = function()
-			local dap = require("dap")
-			local wk = require("which-key")
-			wk.add({
-				{
-					group = "Debug",
-					"<leader>d",
-					{ "<leader>db", dap.toggle_breakpoint, mode = "n", desc = "Toggle breakpoint", icon = { icon = "", color = "white" } },
-					{ "<leader>dc", dap.continue, mode = "n", desc = "Continue", icon = { icon = "", color = "white" } },
-					{ "<F12>", dap.step_into, mode = "n", desc = "Step into", icon = { icon = "", color = "white" } },
+  {
+    "mfussenegger/nvim-dap",
+    event = "VeryLazy",
+    config = function()
+      local dap = require("dap")
+      local wk = require("which-key")
+      wk.add({
+        {
+          group = "Debug",
+          "<leader>d",
+          { "<leader>db", dap.toggle_breakpoint, mode = "n", desc = "Toggle breakpoint", icon = { icon = "", color = "white" } },
+          { "<leader>dc", dap.continue, mode = "n", desc = "Continue", icon = { icon = "", color = "white" } },
+          { "<F12>", dap.step_into, mode = "n", desc = "Step into", icon = { icon = "", color = "white" } },
           { "<F11>", dap.step_over, mode = "n", desc = "Step over", icon = { icon = "", color = "white" } },
-					{ "<F10>", dap.step_out, mode = "n", desc = "Step out", icon = { icon = "", color = "white" } },
-					{ "<leader>dr", dap.run_to_cursor, mode = "n", desc = "Run to cursor", icon = { icon = "", color = "white" } },
-				},
-			})
+          { "<F10>", dap.step_out, mode = "n", desc = "Step out", icon = { icon = "", color = "white" } },
+          { "<leader>dr", dap.run_to_cursor, mode = "n", desc = "Run to cursor", icon = { icon = "", color = "white" } },
+        },
+      })
 
-			dap.adapters.gdb = {
-				type = "executable",
-				command = "gdb",
-				args = { "-i", "dap" },
-			}
-			dap.configurations.asm = {
-				{
-					name = "Debug ASM",
-					type = "gdb",
-					request = "launch",
-					program = function()
-						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-					end,
-					cwd = "${workspaceFolder}",
-					stopOnEntry = true,
-				},
-			}
-		end,
-	},
-	{
-		"rcarriga/nvim-dap-ui",
-		event = "VeryLazy",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-			"nvim-neotest/nvim-nio",
-		},
-		config = function()
-			require("dapui").setup()
-			local dap = require("dap")
-			local dapui = require("dapui")
+      dap.adapters.gdb = {
+        type = "executable",
+        command = "gdb",
+        args = { "-i", "dap" },
+      }
+      dap.configurations.asm = {
+        {
+          name = "Debug ASM",
+          type = "gdb",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+          end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = true,
+        },
+      }
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      require("dapui").setup()
+      local dap = require("dap")
+      local dapui = require("dapui")
 
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
-			end
-		end,
-	},
-	{
-		"jay-babu/mason-nvim-dap.nvim",
-		event = "VeryLazy",
-		opts = {
-			handlers = {},
-		},
-	},
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    opts = {
+      handlers = {},
+    },
+  },
 }
