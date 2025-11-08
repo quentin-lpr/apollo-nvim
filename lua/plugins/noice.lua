5879fb6883b5ccfa73736c2e60da41d50ccf0228
Quentin L. <quentin.lapierre@epitech.eu>
2025-11-08 19:05:19 +0100

    feat(noice): skip jdtls LSP progress messages
    Configure noice.nvim to filter out and skip LSP progress messages from
    the
    jdtls client. This prevents noisy status updates from Java LSP in the
    UI,
    improving clarity for other language servers.

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,5 +1,28 @@
 -- Highly experimental plugin that completely replaces the UI for messages, cmdline and the popupmenu
 return {
   "folke/noice.nvim",
-  opts = {},
+  opts = {
+    lsp = {
+      progress = {
+        enabled = true,
+      },
+    },
+    routes = {
+      {
+        filter = {
+          event = "lsp",
+          kind = "progress",
+        },
+        filter_opts = {
+          cond = function(message)
+            local client = vim.lsp.get_client_by_id(message.opts.client_id)
+            return client and client.name == "jdtls"
+          end,
+        },
+        opts = {
+          skip = true,
+        },
+      },
+    },
+  },
 }
