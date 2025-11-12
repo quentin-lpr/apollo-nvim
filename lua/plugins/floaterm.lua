4bbae459ad1045ff020f1470e23b5a449445c75e
Quentin L. <quentin.lapierre@epitech.eu>
2025-11-12 14:19:21 +0100

    feat(terminal): add floaterm plugin and keymaps
    Introduce the "nvzone/floaterm" plugin for floating terminal management.
    Configure keybindings for toggling and cycling terminals, with
    platform-specific
    mappings for Darwin and others. Remove float terminal logic from
    toggleterm
    plugin to avoid duplication.

---
+++ lua/plugins/floaterm.lua
@@ -0,0 +1,32 @@
+return {
+	"nvzone/floaterm",
+	dependencies = "nvzone/volt",
+	opts = {},
+	config = function(_, opts)
+    require("floaterm").setup(opts)
+		local function next_term()
+			require("floaterm.api").cycle_term_bufs("next")
+		end
+
+		local function prev_term()
+			require("floaterm.api").cycle_term_bufs("prev")
+		end
+
+		local os_name = vim.loop.os_uname().sysname
+		local wk = require("which-key")
+		wk.add({
+			{ "<C-q>", "<C-\\><C-n>", mode = "t", desc = "Exit terminal mode" },
+		})
+		if os_name == "Darwin" then
+			wk.add({
+				{ "@", "<Cmd>FloatermToggle<CR>", mode = { "n", "t" }, desc = "Toggle Terminal", icon = { icon = "", color = "white" } },
+				{ "<M-Up>", prev_term, mode = { "n", "t" }, desc = "Toggle Terminal", icon = { icon = "", color = "white" } },
+				{ "<M-Down>", next_term, mode = { "n", "t" }, desc = "Toggle Terminal", icon = { icon = "", color = "white" } },
+			})
+		else
+			wk.add({
+				{ "²", "<Cmd>FloatermToggle<CR>", mode = { "n", "t" }, desc = "Toggle Terminal", icon = { icon = "", color = "white" } },
+			})
+		end
+	end,
+}
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -32,32 +32,10 @@ return {
       lazygit:toggle()
     end
 
-    -- Float Terminal
-    local terminal = Terminal:new({
-      direction = "float",
-      float_opts = { border = "curved" },
-      on_open = function()
-        vim.cmd("startinsert!")
-      end,
-    })
-    local function toggle_terminal()
-      terminal:toggle()
-    end
-
-    local os_name = vim.loop.os_uname().sysname
     local wk = require("which-key")
     wk.add({
       { "<leader>lg", toggle_lazygit, mode = "n", desc = "Toggle Lazygit Window", icon = { icon = "", color = "white" } },
       { "<C-q>", "<C-\\><C-n>", mode = "t", desc = "Exit terminal mode" },
     })
-    if os_name == "Darwin" then
-      wk.add({
-        { "@", toggle_terminal, mode = { "n", "t" }, desc = "Toggle Terminal", icon = { icon = "", color = "white" } },
-      })
-    else
-      wk.add({
-        { "²", toggle_terminal, mode = { "n", "t" }, desc = "Toggle Terminal", icon = { icon = "", color = "white" } },
-      })
-    end
   end,
 }
